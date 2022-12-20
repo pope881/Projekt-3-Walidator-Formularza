60b8818d732eb948ddc3084b319f5ae6c844f663
Bartosz Papierz <papierz.bartosz@gmail.com>
2022-12-20 20:31:13 +0100

    js file changed

--- script.js
+++ script.js
@@ -43,56 +43,45 @@ const checkPassword = (pass1, pass2) => {
 
 const checkMail = email => {
 	const re =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-        if(re.test(email.value)){
-            clearError(email)
-        } else {
-            showError(email, 'email jest niepoprawny')
-        }
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'email jest niepoprawny')
+	}
 }
-
 
 const checkErrors = () => {
-    const allInputs = document.querySelectorAll('.form-box')
-    let countErrors = 0
+	const allInputs = document.querySelectorAll('.form-box')
+	let countErrors = 0
 
-    allInputs.forEach(el => {
-        if(el.classList.contains('error')) {
-            countErrors++
-        }
-    })
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			countErrors++
+		}
+	})
 
-    if(countErrors === 0) {
-        popup.classList.add('show-popup')
-    }
+	if (countErrors === 0) {
+		popup.classList.add('show-popup')
+	}
 }
 
-
-
-
-
-
-
-
-
 sendBtn.addEventListener('click', e => {
-	e.preventDefault();
+	e.preventDefault()
 
-	checkForm([username, pass, pass2, email]);
-	checkLength(username, 3);
-	checkLength(password, 8);
-	checkPassword(pass, pass2);
-    checkMail(email);
-    checkErrors()
+	checkForm([username, pass, pass2, email])
+	checkLength(username, 3)
+	checkLength(password, 8)
+	checkPassword(pass, pass2)
+	checkMail(email)
+	checkErrors()
 })
-
-
 
 clearBtn.addEventListener('click', e => {
 	e.preventDefault()
 	;[username, pass, pass2, email].forEach(el => {
 		el.value = ''
-        clearError(el)
+		clearError(el)
 	})
 })
